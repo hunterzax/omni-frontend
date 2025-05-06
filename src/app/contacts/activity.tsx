1459e89407b54f024e795a3202bf613260f89a4d
BANGJU <devjuuxo@gmail.com>
2025-05-06 17:13:37 +0700

    merge-user

--- src/app/contacts/activity.tsx
+++ src/app/contacts/activity.tsx
@@ -9,10 +9,71 @@ import NorthIcon from '@mui/icons-material/North';
 import { colors } from "@mui/material";
 
 const ActivityBar: React.FC<any> = ({dataContacts, dataLabels}: any) => {
+
+    const [currentItemMerge, setcurrentItemMerge] = useState<any>();
+    const [selectItemMerge, setselectItemMerge] = useState<any>();
+
     const onSelectHistory = (item: any) => {
         localStorage?.setItem('msgID', item?.id);
         window.location.href = '/dashboard';
     }
+    // <SelectItem value="est" style={{color:"blue"}}>nucha jar (Facebook)</SelectItem>
+    // <SelectItem value="cst" style={{color:"red"}}>Phakawat (Tiktok)</SelectItem>
+    // <SelectItem value="mst" style={{color:"#ed6600"}}>RedBull99 (Shopee)</SelectItem>
+    // <SelectItem value="pst" style={{color:"blue"}}>viton s.(Facebook)</SelectItem>
+    // <SelectItem value="akst" style={{color:"#fca800"}}>Alaska maranu (Lazada)</SelectItem>
+    // <SelectItem value="hst" style={{color:"red"}}>สมศรี ฆ่าหมีด้วยมือเปล่า (Tiktok)</SelectItem>
+
+    let mockUserlist: any = [
+        {
+            id: 1,
+            name: 'nucha jar',
+            labels: {
+                title: 'facebook',
+                color: '#3b82f6'
+            }
+        },
+        {
+            id: 2,
+            name: 'Phakawat',
+            labels: {
+                title: 'Tiktok',
+                color: '#6b7280'
+            }
+        },
+        {
+            id: 3,
+            name: 'RedBull99',
+            labels: {
+                title: 'Shopee',
+                color: '#ef4444'
+            }
+        },
+        {
+            id: 4,
+            name: 'viton s.',
+            labels: {
+                title: 'facebook',
+                color: '#3b82f6'
+            }
+        },
+        {
+            id: 5,
+            name: 'Alaska maranu',
+            labels: {
+                title: 'Lazada',
+                color: '#e9a812'
+            }
+        },
+        {
+            id: 6,
+            name: 'สมศรี ฆ่าหมีด้วยมือเปล่า',
+            labels: {
+                title: 'Tiktok',
+                color: '#6b7280'
+            }
+        },
+    ]
 
     return (
         <div className="bg-white w-full h-full p-4 border-l-[1px] border-gray-200">
@@ -68,20 +129,34 @@ const ActivityBar: React.FC<any> = ({dataContacts, dataLabels}: any) => {
                         <div className="bg-gray-200 text-green-600 px-2 rounded-sm w-[110px] text-center">{'To be saved'}</div>
                     </div>
                     <div className="mt-2 w-full relative">
-                        <Select>
-                            <SelectTrigger className="w-full flex">
-                                <SelectValue />
+                        <Select value={selectItemMerge || null} onValueChange={(e: any) => setselectItemMerge(e)}>
+                            <SelectTrigger className="w-full flex h-[45px]">
+                                {/* <SelectValue /> */}
+                                <SelectValue className="text-black">
+                                    {selectItemMerge ?
+                                        <div className="flex items-center gap-2">
+                                            <div 
+                                                className="w-5 h-5 rounded-full relative bg-cover"
+                                                style={{backgroundImage: `url(${'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzGYOukhtzQwJiFMmFihZEqZBr1wNMkTjgQg&s'})`}}
+                                            />
+                                            <div className="font-[500]">{mockUserlist?.find((itemf: any) => itemf?.id == selectItemMerge)?.name}</div>
+                                        </div>
+                                        :<span className=" opacity-70">{'Search for primary contact'}</span>
+                                    }
+                                </SelectValue>
                             </SelectTrigger>
                             <SelectContent>
                                 <div className="flex items-center border-gray-300 border p-2 rounded-md gap-2"><SearchIcon sx={{fontSize: 14, color: '#60646c'}}/><input placeholder="Search for a contact" className="!shadow-none !border-none !outline-none text-[14px]"/></div>
                                 <SelectGroup>
                                     <SelectLabel>User Account</SelectLabel>
-                                    <SelectItem value="est" style={{color:"blue"}}>nucha jar (Facebook)</SelectItem>
-                                    <SelectItem value="cst" style={{color:"red"}}>Phakawat (Tiktok)</SelectItem>
-                                    <SelectItem value="mst" style={{color:"#ed6600"}}>RedBull99 (Shopee)</SelectItem>
-                                    <SelectItem value="pst" style={{color:"blue"}}>viton s.(Facebook)</SelectItem>
-                                    <SelectItem value="akst" style={{color:"#fca800"}}>Alaska maranu (Lazada)</SelectItem>
-                                    <SelectItem value="hst" style={{color:"red"}}>สมศรี ฆ่าหมีด้วยมือเปล่า (Tiktok)</SelectItem>
+                                    {mockUserlist?.filter((itemf: any) => currentItemMerge ? itemf?.id !== currentItemMerge : itemf?.id !== null)?.map((item: any) => {return(
+                                        <SelectItem className=" cursor-pointer capitalize" style={{backgroundColor: selectItemMerge == item?.id ? 'rgba(59, 130, 246, 0.1)': 'none'}} value={item?.id}>
+                                            <div className="flex items-center gap-2">
+                                                <div className="w-2 h-2 rounded-[2px]" style={{backgroundColor: item?.labels?.color}}></div>
+                                                <div>{item?.name + ` (${item?.labels?.title})`}</div>
+                                            </div>
+                                        </SelectItem>
+                                    )})}
                                 </SelectGroup>
                             </SelectContent>
                         </Select>
@@ -97,10 +172,30 @@ const ActivityBar: React.FC<any> = ({dataContacts, dataLabels}: any) => {
                         <div className="text-black">{'To be merged'}</div>
                         <div className="bg-gray-200 text-red-600 px-2 rounded-sm w-[110px] text-center">{'To be deleted'}</div>
                     </div>
-                    <div className="mt-2 w-full h-[35px] border border-gray-200 rounded-md"></div>
+                    <div className="mt-2 w-full h-[45px] border border-gray-200 rounded-md">
+                        {currentItemMerge &&
+                            <div className="flex items-center gap-2 h-full px-2">
+                                <div 
+                                    className="w-5 h-5 rounded-full relative bg-cover"
+                                    style={{backgroundImage: `url(${'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzGYOukhtzQwJiFMmFihZEqZBr1wNMkTjgQg&s'})`}}
+                                />
+                                <div className="font-[500]">{mockUserlist?.find((itemf: any) => itemf?.id == currentItemMerge)?.name + ` (${mockUserlist?.find((itemf: any) => itemf?.id == currentItemMerge)?.labels?.title})`}</div>
+                            </div>
+                        }
+                    </div>
                     <div className="grid grid-cols-2 gap-2 mt-4">
-                        <Button className="h-[40px] bg-gray-200 hover:bg-gray-300 text-blue-500">Cancle</Button>
-                        <Button className="h-[40px] bg-blue-500 hover:bg-blue-600 text-white">Merge contact</Button>
+                        <Button 
+                            className="h-[40px] bg-gray-200 hover:bg-gray-300 text-blue-500"
+                            onClick={() => {setselectItemMerge(undefined), setcurrentItemMerge(undefined)}}
+                        >
+                            Cancle
+                        </Button>
+                        <Button 
+                            className="h-[40px] bg-blue-500 hover:bg-blue-600 text-white"
+                            onClick={() => {if(selectItemMerge){setcurrentItemMerge(selectItemMerge), setselectItemMerge(undefined)}}}
+                        >
+                            Merge contact
+                        </Button>
                     </div>
                 </div>
             </TabsContent>
